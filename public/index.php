058979e718000914279b3a2fd2db88a420845c61
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-09-02 01:18:25 -0700

    Replace logging with DB storage

--- public/index.php
+++ public/index.php
@@ -1,21 +1,38 @@
 <?php
 
+// Require code from other files.
+require_once __DIR__ . '/../App/Database/Connectors/HttpRequest.php';
+require_once __DIR__ . '/../App/Database/Migrations/HttpRequest.php';
 require_once __DIR__ . '/../App/Entities/HttpRequest.php';
+require_once __DIR__ . '/../App/Entities/SQLite3Database.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Entities\HttpRequest;
+// Import namespaces.
+use App\Database\Connectors\HttpRequest as HttpRequestConnector;
+use App\Database\Migrations\HttpRequest as HttpRequestMigration;
+use App\Entities\HttpRequest as HttpRequestEntity;
+use App\Entities\SQLite3Database;
 
 // Load the dotenv file.
 Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
 
-// Create a new (incoming) HTTP Request object with info from the current request.
-$http_request = new HttpRequest();
-$http_request->populateInfoFromServerVar($_SERVER);
+// Pull configs from dotenv file.
+$db_filepath      = __DIR__ . "/../{$_ENV['database.directory']}/{$_ENV['database.name']}";
+$local_time_zone  = new DateTimeZone($_ENV['time_zone']);
 
-// Format request info and log it to a file.
-$info = $http_request->getInfoTextString();
-$log_directory  = __DIR__ . "/../{$_ENV['log.directory']}";
-$http_request->logInfoStringToTextFile($info, $log_directory);
+// Create a new (incoming) HTTP request entity with info from the current request.
+$http_request_entity = new HttpRequestEntity();
+$http_request_entity->populateInfoFromServerVar($_SERVER, $local_time_zone);
+
+// Configure the database.
+$db_migrations = [
+  new HttpRequestMigration(),
+];
+$database = new SQLite3Database($db_filepath, $db_migrations);
+
+// Save the HTTP request entity.
+$db_connector = new HttpRequestConnector();
+$database->saveEntity($http_request_entity, $db_connector);
 
 // Show the client something.
 require_once __DIR__ . '/../App/Views/Home.php';
