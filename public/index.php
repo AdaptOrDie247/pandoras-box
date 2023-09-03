162bcf46c159748ed5c71fda17cb31569034985f
Daniel Gilbert <121529996+AdaptOrDie247@users.noreply.github.com>
2023-09-02 19:56:06 -0700

    Refactor index to use new code

--- public/index.php
+++ public/index.php
@@ -3,17 +3,21 @@
 // Require code from other files.
 require_once __DIR__ . '/../App/Connectors/HttpRequest.php';
 require_once __DIR__ . '/../App/Database/Migrations/HttpRequest.php';
+require_once __DIR__ . '/../App/Database/Migrations/UniqueVisitor.php';
 require_once __DIR__ . '/../App/Entities/HttpRequest.php';
 require_once __DIR__ . '/../App/Models/BaseModel.php';
 require_once __DIR__ . '/../App/Models/HttpRequest.php';
+require_once __DIR__ . '/../App/Models/UniqueVisitor.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Import namespaces.
 use App\Connectors\HttpRequest as HttpRequestConnector;
 use App\Database\Migrations\HttpRequest as HttpRequestMigration;
+use App\Database\Migrations\UniqueVisitor as UniqueVisitorMigration;
 use App\Entities\HttpRequest as HttpRequestEntity;
 use App\Models\BaseModel;
 use App\Models\HttpRequest as HttpRequestModel;
+use App\Models\UniqueVisitor as UniqueVisitorModel;
 
 // Load the dotenv file.
 Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
@@ -27,6 +31,7 @@ $http_request_entity->populateInfoFromServerVar($_SERVER, $local_time_zone);
 $db_filepath    = __DIR__ . "/../{$_ENV['database.directory']}/{$_ENV['database.name']}";
 $db_migrations  = [
   new HttpRequestMigration(),
+  new UniqueVisitorMigration(),
 ];
 $database = new BaseModel($db_filepath);
 $database->runMigrations($db_migrations);
@@ -35,6 +40,9 @@ $database->runMigrations($db_migrations);
 $http_request_model     = new HttpRequestModel();
 $http_request_connector = new HttpRequestConnector();
 $database->saveEntity($http_request_entity, $http_request_model, $http_request_connector);
+
+// Save unique visitor entity if applicable.
+$unique_visitor_model = new UniqueVisitorModel($db_filepath);
 
 // Show the client something.
 require_once __DIR__ . '/../App/Views/Home.php';
